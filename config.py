661e90978aba8279f736e4471e92bed93c059043
yizt <csuyzt@163.com>
2019-05-07 10:42:54 +0800

    初始化目录

---
+++ config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+   File Name：     config.py
+   Description :   配置类
+   Author :       mick.yi
+   Date：          2019/5/7
+"""
+
+
+class Config(object):
+    ANCHOR_SCALES = [2, 3, 4]
+    BATCH_SIZE = 1
+
+
+cur_config = Config()
---
+++ preprocess/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+   File Name：     __init__.py.py
+   Description :  
+   Author :       mick.yi
+   Date：          2019/5/7
+"""
