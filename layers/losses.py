3c0817b9d23d33493dcf7d1d65d4d458d052b8d1
yizt <csuyzt@163.com>
2019-05-09 14:56:58 +0800

    rpn 回归损失

--- layers/losses.py
+++ layers/losses.py
@@ -23,8 +23,20 @@ def rpn_cls_loss(anchors_tag, predict_logits):
     return loss
 
 
-def rpn_regress_loss():
-    pass
+def rpn_regress_loss(deltas, predict_deltas, anchors_tag):
+    """
+    rpn 回归损失
+    :param deltas: 真实回归目标torch tensor [batch,anchors_num,(dy,dx,dz,dh,dw,dd)]
+    :param predict_deltas: 预测回归目标torch tensor [batch,anchors_num,(dy,dx,dz,dh,dw,dd)]
+    :param anchors_tag: torch tensor [batch,anchors_num]  1-正样本，-1-负样本，0-不参与训练
+    :return:
+    """
+    # 获取训练的anchors的索引号
+    ix = (anchors_tag == 1).nonzero()  # 只有正样本回归
+    deltas = deltas[ix[:, 0], ix[:, 1]]  # [n,(dy,dx,dz,dh,dw,dd)]
+    predict_deltas = predict_deltas[ix[:, 0], ix[:, 1]]  # [n,(dy,dx,dz,dh,dw,dd)]
+    loss = F.smooth_l1_loss(predict_deltas, deltas)  # 标量
+    return loss
 
 
 def mrcnn_cls_loss():
