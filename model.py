bb2b6cf0f215d0070a34a204d9bc9a16cf115f96
ybcc <271380178@qq.com>
2019-05-10 16:38:21 +0800

    增加注释

--- model.py
+++ model.py
@@ -18,8 +18,14 @@ from layers.losses import rpn_cls_loss, rpn_regress_loss, mrcnn_cls_loss, mrcnn_
 
 class LungNet(nn.Module):
     def __init__(self, cfg, parse="train"):
+        """
+
+        :param cfg: 配置上下文
+        :param parse: "train" or "test"
+        """
         super(LungNet, self).__init__()
         self.cfg = cfg
+        self.phase = parse
         self.base_net = Net(cfg.NUM_ANCHORS)
         self.rpn_head = RpnHead(cfg.NUM_ANCHORS)
         self.mrcnn_head = MrcnnHead(cfg)
@@ -33,42 +39,51 @@ class LungNet(nn.Module):
 
     def forward(self, x, gt_boxes, gt_labels):
         """
-
+        前向传播
         :param x: tensor, [Batch, Channel, D, H, W]
         :param gt_boxes: list of numpy [n,(y1,x1,z1,y2,x2,z2)]
         :param gt_labels: list of numpy [n]
         :return:
         """
+        # 获取feature map
         feature_map = self.base_net(x)
 
+        # 获取rpn的输出
         rpn_output = self.rpn_head(feature_map)
         predict_scores, predict_deltas = rpn_output[:, :, -1], rpn_output[:, :, :-1]
-        batch_anchors_tag, batch_deltas = self.rpn_target(self.anchors, gt_boxes, gt_labels)
+        # 获取anchors的真实类别和真实偏移量
+        gt_anchors_tag, gt_anchors_deltas = self.rpn_target(self.anchors, gt_boxes, gt_labels)
 
-        # rpn阶段loss
-        cls_loss_rpn = rpn_cls_loss(batch_anchors_tag, predict_scores)
-        regr_loss_rpn = rpn_regress_loss(batch_deltas, predict_deltas, batch_anchors_tag)
+        # 计算rpn阶段loss
+        cls_loss_rpn = rpn_cls_loss(gt_anchors_tag, predict_scores)
+        regr_loss_rpn = rpn_regress_loss(gt_anchors_deltas, predict_deltas, gt_anchors_tag)
 
+        # 获取proposals
         batch_proposals, batch_scores, batch_indices = self.proposal(self.anchors, predict_scores, predict_deltas)
-
-        batch_rois, batch_deltas, batch_labels, rois_indices = self.mrcnn_traget(batch_proposals, batch_indices,
+        # 获取mrcnn target
+        batch_rois, gt_deltas, gt_labels, rois_indices = self.mrcnn_traget(batch_proposals, batch_indices,
                                                                                  gt_boxes, gt_labels)
+        # 维度处理
         rois_indices = torch.unsqueeze(rois_indices, dim=1)
         rois = torch.cat((batch_rois, rois_indices), dim=1)
+        # roi align
         rois = self.roi_align(feature_map, rois)
 
+        # 获取mrcnn head输出
         predict_mrcnn_cls, predict_mrcnn_regr, predict_mask = self.mrcnn_head(rois)
 
-        # mrcnn阶段loss
-        cls_loss_mrcnn = mrcnn_cls_loss(batch_labels, predict_mrcnn_cls)
-        regr_loss_mrcnn = mrcnn_regress_loss(batch_deltas, predict_mrcnn_regr, batch_labels)
+        # 计算mrcnn阶段loss
+        cls_loss_mrcnn = mrcnn_cls_loss(gt_labels, predict_mrcnn_cls)
+        regr_loss_mrcnn = mrcnn_regress_loss(gt_deltas, predict_mrcnn_regr, gt_labels)
         gt_mask = None  # todo: 真实的mask将从mrcnn_target中返回，待完成
-        mask_loss = mrcnn_mask_loss(gt_mask, predict_mask, batch_labels)
+        mask_loss = mrcnn_mask_loss(gt_mask, predict_mask, gt_labels)
 
         # 总的Loss
         self.total_loss = cls_loss_rpn + regr_loss_rpn + cls_loss_mrcnn + regr_loss_mrcnn + mask_loss
 
-        return predict_mrcnn_cls, predict_mrcnn_regr, predict_mask
+        if self.phase == 'test':
+            # todo: 预测过程
+            pass
 
     # property装饰的方法可以直接当作属性调用
     @property
