2f241630eccc50309085a2366054ec24c7380054
yizt <csuyzt@163.com>
2019-05-10 17:06:07 +0800

    重构roialgin

--- layers/roialign.py
+++ layers/roialign.py
@@ -13,23 +13,31 @@ from cuda_functions.roi_align_3D.roi_align.crop_and_resize import CropAndResizeF
 
 class RoiAlign(nn.Module):
     # pool_size_h, pool_size_w, pool_size_t, image_size均为整数，不能是torch tensor
-    def __init__(self, pool_size_h, pool_size_w, pool_size_t, image_size):
+    def __init__(self, pool_size_h, pool_size_w, pool_size_d, image_size):
+        """
+
+        :param pool_size_h: 池化后的高度
+        :param pool_size_w: 池化后的宽度
+        :param pool_size_d: 池化后的厚度
+        :param image_size: 图像尺寸，默认图像各维度尺寸一致
+        """
         self.pool_size_h = pool_size_h
         self.pool_size_w = pool_size_w
-        self.pool_size_t = pool_size_t
+        self.pool_size_d = pool_size_d
         self.image_size = image_size
         super(RoiAlign, self).__init__()
 
-    def forward(self, features, rois):
+    def forward(self, features, rois, rois_indices):
         """
 
         :param: features [batch,C,H,W,T]
-        :param: rois [roi_num,(y1,x1,z1,y2,x2,z2,batch_index)]
-        :return: 池化后的特征  [roi_num,C,ph,pw,pt]
+        :param: rois [roi_num,(y1,x1,z1,y2,x2,z2)]
+        :param: rois_indices [roi_num]  指示roi属于batch中哪个样本
+        :return: 池化后的特征  [roi_num,C,ph,pw,pt]  pt代表pool_size_h
 
         """
         # 坐标归一化
-        boxes = rois[:, :6] / self.image_size
-        batch_indices = rois[:, 6].int()
+        boxes = rois / self.image_size
+        batch_indices = rois_indices.int()
         x = crop_and_resize(self.pool_size_h, self.pool_size_w, self.pool_size_t, 0)(features, boxes, batch_indices)
         return x
